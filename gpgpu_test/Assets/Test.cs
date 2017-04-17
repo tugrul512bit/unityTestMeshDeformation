accf0891503b1348bcb7b111d5e763e93484c6cf
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2017-04-17 12:17:02 +0300

    Add files via upload

---
+++ gpgpu_test/Assets/Test.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+    public static int sayac { get; set; }
+    public static System.Random r { get; set; }
+    // Use this for initialization
+    public static Material m=null;
+    void Start () {
+        if (r == null)
+            r = new System.Random();
+        sayac++;
+        if (m == null)
+            m = GetComponent<Renderer>().sharedMaterial;
+        else
+            GetComponent<Renderer>().material = m;
+
+    }
+
+
+}
